f1662dacb1f68ed547b6a562bfa7ad56ac9305e5
DustinHart <dustin.hart@dpsnc.net>
2015-09-28 09:46:08 -0400

    Adding to GitHub

---
+++ artScript.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$(function() {
+		$("#title").load("content.html .html .title");
+		$("#info").load("content.html .html .info");
+		$("#image").load("content.html .css .image");
+		$("#post").load("content.html .html .post");
+	});
+});
